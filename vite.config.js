c9c3c917f2d4656d9c73174cdb85c834b4c9766e
Floware <rafaxtz3@gmail.com>
2024-09-05 00:14:16 -0600

    FIx for GH Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://floware.me', // Link page
+  base: 'https://floware.me/Floware-front', // Link page
 });
